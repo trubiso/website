20212db3ad83648351ba1fcfde9ea26271fdfda8
trubiso <crishipohola@gmail.com>
2021-10-10 18:50:43 +0200

    the

--- src/routes/socials.svelte
+++ src/routes/socials.svelte
@@ -1,3 +1,40 @@
+<script lang="ts">
+    type SM = {
+            name: string,
+        username: string,
+           link?: string
+    };
+    const smc : SM[] = [
+        {
+            name: "github",
+            username: "trubiso",
+            link: "https://www.github.com/trubiso"
+        },
+        {
+            name: "twitch",
+            username: "trubiso",
+            link: "https://www.twitch.tv/trubiso"
+        },
+        {
+            name: "discord",
+            username: "@Trubiso#5265"
+        }
+    ]
+</script>
+
 <svelte:head>
     <title>socials</title>
 </svelte:head>
+
+<section>
+    <h1>here's my stuff</h1>
+    <ul>
+        {#each smc as s}
+            <li>{s.name}: {#if s.link}
+                <a href="{s.link}" target="_blank">{s.username}</a>
+            {:else}
+                <span>{s.username}</span>
+            {/if}</li>
+        {/each}
+    </ul>
+</section>
