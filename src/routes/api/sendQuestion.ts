2f83cdaaa34006e0157f356bc9940e41b6358e2b
trubiso <trubiso@users.noreply.github.com>
2022-02-27 15:48:22 +0100

    h

--- src/routes/api/sendQuestion.ts
+++ src/routes/api/sendQuestion.ts
@@ -1,7 +1,7 @@
 import prisma from '$lib/db';
 import type { RequestHandler } from '@sveltejs/kit';
 
-export const get: RequestHandler = () => {
+export const get: RequestHandler = async () => {
 	return {
 		status: 200,
 		body: {
