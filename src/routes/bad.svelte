7a5d7a77791ba583763ae237c2984dace7877ff7
trubiso <trubiso@users.noreply.github.com>
2022-03-05 16:32:25 +0100

    fixed the bad page

--- src/routes/bad.svelte
+++ src/routes/bad.svelte
@@ -1,43 +1,77 @@
 <script lang="ts">
-  const ss = (a: string) =>
-    document.getElementsByTagName('section')
-      ? (document.getElementsByTagName('section')[0].innerHTML = a)
-      : 1;
-  const cln = () => {
-    ss(
-      "<h1>why didn't you listen to me</h1>\
-            <br>that's rude<span style='opacity: 2%; cursor: default;'> (or is it? use 'dQw4w9WgXcQ')</span>"
-    );
-  };
-  const lp = () => {
-    document.getElementById('huh').parentElement.innerHTML +=
-      "<br><img src='https://cdn.discordapp.com/emojis/717683480787550228.png' alt='h' />";
-    setTimeout(lp, 1000);
-  };
-  const nl = () => {
-    if ((document.getElementById('huh') as HTMLInputElement)?.value === 'dQw4w9WgXcQ') lp();
-  };
+  import Emote from '$lib/emote.svelte';
+
+  import { lang } from '$lib/stores';
+
+  import { getTitleOfPage } from '$lib/vars';
+
+  let phase = 0,
+    text = '',
+    smilieCount = 0;
+  const requiredText = 'dQw4w9WgXcQ';
+
+  function textPhase() {
+    if (text === requiredText) {
+      phase++;
+      function incrementSmilies() {
+        smilieCount++;
+        setTimeout(incrementSmilies, 1000);
+      }
+      incrementSmilies();
+    }
+  }
 </script>
 
 <svelte:head>
-  <title>don't cliq</title>
+  <title>{getTitleOfPage("don't click", $lang)}</title>
 </svelte:head>
 
 <main>
-  THIS IS A VERY BAD PAGE. LEAVE IT IMMEDIATELY.
-  <br /><br /><br /><br />(there's nothing more to it)
-  <br /><br /><br /><br />(just leave it)
-  <br /><br /><br /><br />
-  <br /><br /><br /><br />(please)
-  <br /><br /><br /><br />
-  <br /><br /><br /><br />
-  <br /><br /><br /><br />(don't waste your time)
-  <br /><br /><br /><br />
-  <br /><br /><br /><br />
-  <br /><br /><br /><br />
-  <br /><br /><br /><br /><span on:click={cln} style="cursor: pointer;">(leave now)</span>
+  {#if phase === 0}
+    THIS IS A VERY BAD PAGE. LEAVE IT IMMEDIATELY.
+    <span class="whitespace" />
+    (there's nothing more to it)
+    <span class="whitespace" />
+    (just leave it)
+    <span class="whitespace" />
+    <span class="whitespace" />
+    (please)
+    <span class="whitespace" />
+    <span class="whitespace" />
+    <span class="whitespace" />
+    (don't waste your time)
+    <span class="whitespace" />
+    <span class="whitespace" />
+    <span class="whitespace" />
+    <span class="whitespace" />
+    <span on:click={() => phase++} style="cursor: pointer;">(leave now)</span>
+  {:else if phase === 1}
+    <h1>why didn't you listen to me</h1>
+    <br />that's rude <br /><span class="hidden">(or is it? use 'dQw4w9WgXcQ')</span><br>
+    <input type="text" bind:value={text} on:change={textPhase} />
+  {:else if phase === 2}
+    <div class="infinite-smilies">
+      {#each [...Array(smilieCount)] as _}
+        <Emote name="happy" size="128" /><br />
+      {/each}
+    </div>
+  {/if}
 </main>
 
-<main id="h">
-  <input type="text" id="huh" value="" on:change|preventDefault={nl} />
-</main>
+<style lang="scss">
+  .hidden {
+    color: var(--bg);
+    cursor: default;
+  }
+
+  .whitespace {
+    display: block;
+    height: 70px;
+  }
+
+  @media (max-width: 600px) {
+    main {
+      text-align: center;
+    }
+  }
+</style>
