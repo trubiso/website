46f8f601ca490782de62ee86c9bee26cb36f1853
trubiso <trubiso@users.noreply.github.com>
2022-03-26 16:50:46 +0100

    frinq

--- src/lib/meta.svelte
+++ src/lib/meta.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   export let title = 'trubiso';
+  export let metatitle: string | null = null;
   export let description: string | null = null;
 </script>
 
 <title>{title}</title>
-<meta content={title} property="og:title" />
+<meta content={metatitle ?? title} property="og:title" />
 {#if description} <meta content={description} property="og:description" /> {/if}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,7 +8,7 @@
   const texts = getTextCollection('index.texts');
 </script>
 
-<Meta title="trubiso" description="come have fun" />
+<Meta title={getTitleOfPage('home', $lang)} metatitle='trubiso' description="come have fun" />
 
 <main>
   <h1>{texts[$lang][0]} <Emote name="happy" size="44" /></h1>
