d807e53065c438b3eb8ced8d79dcdaec3087305e
trubiso <trubiso@users.noreply.github.com>
2022-04-16 00:52:24 +0200

    theme system change

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,33 +7,16 @@
   import { onMount } from 'svelte';
   import '../styles/globals.scss';
 
-  let ct: ITheme | string = 'q';
+  let ct: ITheme | string = themeColors.smilie;
 
   onMount(() => {
     const lt = localStorage.getItem('theme');
-    if (!lt) theme.set('smilie');
-    else if (lt.includes('{')) theme.set(JSON.parse(lt));
-    else theme.set(lt);
+    theme.set(lt ? JSON.parse(lt) : themeColors.smilie);
     theme.subscribe((v) => {
-      if (typeof $theme === 'string') {
-        if (typeof ct !== 'string' || ct === 'q') {
-          document.documentElement.setAttribute('style', '');
-          document.documentElement.classList.add(($theme as string).replace(/_/, '-'));
-        } else {
-          document.documentElement.classList.replace(
-            (ct as string).replace(/_/, '-'),
-            $theme.replace(/_/, '-')
-          );
-        }
-      } else {
-        if (typeof ct === 'string') {
-          document.documentElement.classList.remove((ct as string).replace(/_/, '-'));
-        }
-        document.documentElement.setAttribute(
-          'style',
-          `--navbar-bg1: ${$theme.navbarBG1}; --navbar-bg2: ${$theme.navbarBG2}; --navbar-text: ${$theme.navbarText}; --navbar-accent: ${$theme.navbarAccent}; --accent: ${$theme.accent}; --bg: ${$theme.bg}; --text: ${$theme.text};`
-        );
-      }
+      document.documentElement.setAttribute(
+        'style',
+        `--navbar-bg1: ${$theme.navbarBG1}; --navbar-bg2: ${$theme.navbarBG2}; --navbar-text: ${$theme.navbarText}; --navbar-accent: ${$theme.navbarAccent}; --accent: ${$theme.accent}; --bg: ${$theme.bg}; --text: ${$theme.text};`
+      );
 
       ct = v;
       localStorage.setItem('theme', typeof ct === 'string' ? ct : JSON.stringify(ct));
