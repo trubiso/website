f5d24ba7403108f11f0697e3f8390ea88dc98c5e
trubiso <trubiso@users.noreply.github.com>
2022-09-05 23:32:24 +0200

    prepared for "production"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,51 @@
+<script lang="ts">
+	import './style.scss';
+</script>
+
 <main>
-  <h1>hai</h1>
-  <p>dis is mai websiet !</p>
-  {#each "gibfjosjbkxfnjkbsdfnjkgbn".split('') as letter}
-    <h1>{letter}</h1>
-    <p>{letter.repeat(48)}</p>
-  {/each}
+	<h1>dev.trubiso.tk</h1>
+	<!-- svelte-ignore a11y-distracting-elements -->
+	<marquee class="big flashing flashy silly">UNSTAEBL !!! BE CARFUL !!!</marquee>
+	<p>
+		<b class="flashing silly">WARNING:</b> if you do not know what you are doing please go back to
+		<span class="big">https://trubiso.tk</span>, this is an <b>experimental</b> version of the page and
+		features might not be 100% polished
+	</p>
+	<p>
+		with that out of the way, if you find any bugs, please report them to me, in the q+a page or
+		personally :)
+	</p>
+	<p>if the warnings scared you here's some silly faces to calm you down</p>
+	<br />
+	<img
+		src="https://cdn.discordapp.com/emojis/729411456491323412.png?size=1024&quality=lossless"
+		alt=":silly:"
+		class="flashy"
+	/><img
+		src="https://cdn.discordapp.com/emojis/729411456491323412.png?size=1024&quality=lossless"
+		alt=":silly:"
+		class="scaly"
+	/><img
+		src="https://cdn.discordapp.com/emojis/729411456491323412.png?size=1024&quality=lossless"
+		alt=":silly:"
+		class="ultrasilly"
+	/>
+	<br />
+	<p class="silly">HEHHEWHEAHWEHHWE</p>
+	<br /><br /><br /><br /><br />
+	<p>finished pages:</p>
+	<ul>
+		<li><a href="/">homepage</a></li>
+		<li>
+			<a href="/hsvtester">hsv tester (technically not a page but just a test for a huge feature)</a
+			>
+		</li>
+		<li><a href="/options">options</a></li>
+	</ul>
+	<p>
+		<b>BEWARE</b> of the sidebar, every link but home and options is broken, the website might feel bad
+		if you click them :(
+	</p>
+	<p>but don't beware too much of the sidebar, it has cool new options as well :)</p>
+	<p>also thanks for sticking around, i hope you didn't die in the pool of bugs :P</p>
 </main>
