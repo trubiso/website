a461c745e5f23f7ed6b7f07a041acc1f89811b5e
trubiso <trubiso@users.noreply.github.com>
2022-12-04 13:52:14 +0100

    more or less about page

--- src/lib/json.ts
+++ src/lib/json.ts
@@ -4,6 +4,7 @@ import * as langsJSON from '../json/langs.json';
 import * as stringsJSON from '../json/strings.json';
 import * as navbarJSON from '../json/navbar.json';
 import * as orderJSON from '../json/order.json';
+import * as socialMediaJSON from '../json/socialmedia.json';
 import type { ThemeProp } from './types';
 
 export const emotes = emotesJSON;
@@ -11,4 +12,10 @@ export const themes = themesJSON;
 export const langs = langsJSON;
 export const strings: Record<string, Record<string, Record<string, string>>> = stringsJSON;
 export const navbar = navbarJSON; // no log_in in navbar.order because it's a special element
-export const order = orderJSON as {"themes": string[], "langs": string[], "themeProps": ThemeProp[]}; // navbar not there because already in navbar.json
+export const order = orderJSON as {
+	themes: string[];
+	langs: string[];
+	socialMedia: ('github' | 'twitch' | 'discord' | 'newgrounds')[];
+	themeProps: ThemeProp[];
+}; // navbar not there because already in navbar.json
+export const socialMedia = socialMediaJSON;
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { order, socialMedia } from '$lib/json';
+	import { t } from '$lib/localization';
+</script>
+
+<main class="about">
+	<h1>{t('about.thanks')}</h1>
+	<p><b>{t('about.privacyPre')}</b> {t('about.privacy')}</p>
+	<p><b>{t('about.collaborationPre')}</b> {t('about.collaboration')}</p>
+	<p><b>{t('about.specialThanksPre')}</b> {t('about.specialThanks')}</p>
+	<p>
+		<b>{t('about.repository')}</b>
+		<a href="https://github.com/trubiso/website">{t('socialMedia.github')}</a>
+	</p>
+	<p><b>{t('about.socialMedia')}</b></p>
+	<ul>
+		{#each order.socialMedia as media}
+			<li>
+				{t(`socialMedia.${media}`)}: {#if socialMedia.links[media]}
+					<a href={socialMedia.values[media]}>{socialMedia.values[media]}</a>
+				{:else}
+					{socialMedia.values[media]}
+				{/if}
+			</li>
+		{/each}
+	</ul>
+	<p><b>{t('about.signoff')}</b></p>
+</main>
