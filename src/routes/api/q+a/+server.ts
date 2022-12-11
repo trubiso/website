c386a7310684f9c75d87db78176e928a0dcbf4ad
trubiso <trubiso@users.noreply.github.com>
2022-12-11 19:05:13 +0100

    more proper response in q+a, maybe fixes stuff

--- src/routes/api/q+a/+server.ts
+++ src/routes/api/q+a/+server.ts
@@ -11,16 +11,22 @@ export const GET = async ({ url }: RequestEvent) => {
 		orderBy: { created_at: 'desc' }
 	});
 
-  const startQuestion = (page - 1) * questionsPerPage;
-  const endQuestion = page * questionsPerPage;
-  const questionList = questions.slice(startQuestion, endQuestion);
+	const startQuestion = (page - 1) * questionsPerPage;
+	const endQuestion = page * questionsPerPage;
+	const questionList = questions.slice(startQuestion, endQuestion);
 
-  const hasPreviousPage = page > 1;
-  const hasNextPage = questions.length > page * questionsPerPage;
+	const hasPreviousPage = page > 1;
+	const hasNextPage = questions.length > page * questionsPerPage;
 
-  return new Response(JSON.stringify({
-    questions: questionList,
-    hasPreviousPage,
-    hasNextPage
-  }));
+	return new Response(
+		JSON.stringify({
+			questions: questionList,
+			hasPreviousPage,
+			hasNextPage
+		}),
+		{
+      status: 200,
+			headers: new Headers({ 'Content-Type': 'application/json' })
+		}
+	);
 };
