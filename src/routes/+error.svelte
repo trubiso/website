84a1ada777267e80751e029c1161911c7da6d3c0
trubiso <trubiso@users.noreply.github.com>
2022-12-29 15:04:20 +0100

    changed error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,3 +1,14 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import EmoteZone from '$lib/text/EmoteZone.svelte';
+</script>
+
 <main>
-  AHHHHHHHHHE ERROREEEEEEEEEE !!! HLEP MEEEEEEEEEEEEEEEEe!! :cri:
+  <h1>O NO!</h1>
+	<p><EmoteZone>AHHHHHHHHHE ERROREEEEEEEEEE !!! HLEP MEEEEEEEEEEEEEEEEe!! :cri:</EmoteZone></p>
+  <hr>
+	<p><b>Code:</b> {$page.status}</p>
+  <pre>{$page.error.message}</pre>
+  <hr>
+  <p><EmoteZone>:please: plz report to trunbiso (wif screenshot or copyinge, to disqord prenferabli). he wil tri to fiks :excited:</EmoteZone></p>
 </main>
