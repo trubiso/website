3574a31250e89906ed323f326aee43fd505192ac
Trubiso <trubiso@users.noreply.github.com>
2023-02-17 11:23:19 +0100

    ok please

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,9 +1,6 @@
-import { PrismaClient as PrismaClientEdge } from '@prisma/client/edge';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client/edge';
 
-const prisma = new (
-	process && process.env.IS_LOCAL && process.env.IS_LOCAL === 'true' ? PrismaClient : PrismaClientEdge
-)();
+const prisma = new PrismaClient(); // this doesn't work locally but i have no way to check whether this is local (otherwise the website just crashes here)
 
 prisma.$connect().then(() => console.log('Connected to Prisma'));
 
