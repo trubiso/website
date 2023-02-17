d7d7b3ee01beda9a6aa50e93414f8a2569c2438e
Trubiso <trubiso@users.noreply.github.com>
2023-02-17 11:21:16 +0100

    oops

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -2,7 +2,7 @@ import { PrismaClient as PrismaClientEdge } from '@prisma/client/edge';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new (
-	process.env.IS_LOCAL && process.env.IS_LOCAL === 'true' ? PrismaClient : PrismaClientEdge
+	process && process.env.IS_LOCAL && process.env.IS_LOCAL === 'true' ? PrismaClient : PrismaClientEdge
 )();
 
 prisma.$connect().then(() => console.log('Connected to Prisma'));
