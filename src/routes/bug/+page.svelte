b66760a033a0e60f93529a0b63d24402ff01dfb8
trubiso <33161454+trubiso@users.noreply.github.com>
2023-04-06 23:07:29 +0200

    basic solved segregation for issues

--- src/routes/bug/+page.svelte
+++ src/routes/bug/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { localizeTag } from '$lib/bug';
 	import { t } from '$lib/localization';
 	import type { bug } from '@prisma/client';
 	import Bug from './Bug.svelte';
@@ -9,6 +10,9 @@
 		bugs: bug[];
 		tag: string | null;
 	};
+
+	$: unsolved = data.bugs.filter((x) => !x.tags.includes('prioritySolved'));
+	$: solved = data.bugs.filter((x) => x.tags.includes('prioritySolved'));
 </script>
 
 <main class="bugs">
@@ -16,11 +20,24 @@
 
 	<BugSubmitForm />
 
-	<div class="bug-grid">
-		{#each data.bugs as bug}
-			{#key data.bugs}
-				<Bug {bug} />
-			{/key}
-		{/each}
+	<div class="unsolved">
+		<div class="bug-grid">
+			{#each unsolved as bug}
+				{#key unsolved}
+					<Bug {bug} />
+				{/key}
+			{/each}
+		</div>
+	</div>
+
+	<div class="solved">
+		<h1>{$localizeTag('prioritySolved')}</h1>
+		<div class="bug-grid">
+			{#each solved as bug}
+				{#key solved}
+					<Bug {bug} />
+				{/key}
+			{/each}
+		</div>
 	</div>
 </main>
