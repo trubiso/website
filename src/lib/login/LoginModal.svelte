b37e15e1a9e62972535717f247f656351bdcb5f8
trubiso <33161454+trubiso@users.noreply.github.com>
2023-04-09 18:15:28 +0200

    cool looking transition for login box

--- src/lib/login/LoginModal.svelte
+++ src/lib/login/LoginModal.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { lock } from '$lib/stores';
 	import { createEventDispatcher, onMount } from 'svelte';
+	import { cubicOut } from 'svelte/easing';
+	import { fade, fly } from 'svelte/transition';
 	import './LoginModal.scss';
 	import LoginModalForm from './LoginModalForm.svelte';
 
@@ -17,8 +19,8 @@
 </script>
 
 <main class="login-modal">
-	<div class="obstruct" on:click={close} />
-	<div class="form">
+	<div class="obstruct" on:click={close} transition:fade={{ duration: 400, easing: cubicOut }} />
+	<div class="form" transition:fly={{ y: 1000 }}>
 		<!--<button on:click={close} style="z-index: 200;">I am done !</button>-->
 		<LoginModalForm on:login on:loginSuccess={close} />
 	</div>
