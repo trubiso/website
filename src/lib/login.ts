74d3b079a3fe2781f9bc2a7043fb6ac57e79311b
trubiso <33161454+trubiso@users.noreply.github.com>
2023-04-09 13:30:41 +0200

    simplified login/logout code
    also made checkLogin for other functions so that no one can alter their username and appear to be someone who they are not

---
+++ src/lib/login.ts
@@ -0,0 +1,81 @@
+import prisma from '$lib/server/db';
+import type { user } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+import cookie from 'cookie';
+import * as uuid from 'uuid';
+
+export async function findToken(token: string, tokens: string[]): Promise<string | null> {
+	let foundToken: string | null = null;
+	for (const userToken of tokens) {
+		if (await bcrypt.compare(token, userToken)) {
+			foundToken = userToken;
+			break;
+		}
+	}
+	return foundToken;
+}
+
+// this is a one-off class for checkLogin
+export class CheckedLogin {
+	public constructor(public user: user, public foundToken: string) {}
+}
+
+// if this function returns a checked login, the login is valid
+export async function checkLogin(
+	username: string,
+	sentToken: string
+): Promise<CheckedLogin | null> {
+	const user = await prisma.user.findFirst({
+		where: {
+			username
+		}
+	});
+
+	if (user === null) {
+		return null;
+	}
+
+	const foundToken = await findToken(sentToken, user.token);
+	if (foundToken === null) {
+		return null;
+	}
+
+	return new CheckedLogin(user, foundToken);
+}
+
+export function makeCookie(name: string, data: string | null, baseDate: number | null): string {
+	const date = baseDate ?? Date.now();
+	if (data === null)
+		return cookie.serialize(name, '', {
+			secure: true,
+			expires: new Date(date - 1000)
+		});
+	return cookie.serialize(name, data, {
+		secure: true,
+		expires: new Date(date + 1000 * 3600 * 24 * 30)
+	});
+}
+
+export async function hash(data: string | Buffer): Promise<string> {
+	return await bcrypt.hash(data, await bcrypt.genSalt(10));
+}
+
+export async function randomHash(): Promise<string> {
+	return await hash(uuid.v4());
+}
+
+export function reply(body: object, status: number, headers?: HeadersInit): Response {
+	return new Response(JSON.stringify({ body }), { status, headers });
+}
+
+export function replyError(error: string, message: string): Response {
+	return reply({ error, message }, 500);
+}
+
+export function replyWithCookies(
+	message: string,
+	usernameCookie: string,
+	tokenCookie: string
+): Response {
+	return reply({ message, usernameCookie }, 200, { 'Set-Cookie': tokenCookie });
+}
--- src/routes/api/user/log/+server.ts
+++ src/routes/api/user/log/+server.ts
@@ -1,33 +1,16 @@
 import { getRequestCookie } from '$lib/functions';
+import {
+	checkLogin,
+	findToken,
+	hash,
+	makeCookie,
+	randomHash,
+	replyError,
+	replyWithCookies
+} from '$lib/login';
 import prisma from '$lib/server/db';
 import type { RequestHandler } from '@sveltejs/kit';
 import * as bcrypt from 'bcrypt';
-import cookie from 'cookie';
-import * as uuid from 'uuid';
-
-async function findToken(token: string, tokens: string[]): Promise<string | null> {
-	let foundToken: string | null = null;
-	for (const userToken of tokens) {
-		if (await bcrypt.compare(token, userToken)) {
-			foundToken = userToken;
-			break;
-		}
-	}
-	return foundToken;
-}
-
-function makeCookie(name: string, data: string | null, baseDate: number | null): string {
-	const date = baseDate ?? Date.now();
-	if (data === null)
-		return cookie.serialize(name, '', {
-			secure: true,
-			expires: new Date(date - 1000)
-		});
-	return cookie.serialize(name, data, {
-		secure: true,
-		expires: new Date(date + 1000 * 3600 * 24 * 30)
-	});
-}
 
 // LOG IN
 export const POST: RequestHandler = async ({ request }) => {
@@ -39,79 +22,32 @@ export const POST: RequestHandler = async ({ request }) => {
 
 		const sentToken = getRequestCookie(request, 'token');
 
-		const user = await prisma.user.findFirst({
-			where: {
-				username: username
-			}
-		});
+		const user = await prisma.user.findFirst({ where: { username } });
 
-		if (!user) {
-			return new Response(
-				JSON.stringify({
-					body: {
-						message: `User not found.`,
-						error: 'username'
-					}
-				}),
-				{ status: 500 }
-			);
-		}
+		if (!user) return replyError('username', 'User not found.');
 
 		if (sentToken)
-			if ((await findToken(sentToken, user.token)) !== null) {
-				return new Response(
-					JSON.stringify({
-						body: {
-							message: 'Already logged in.',
-							error: 'ok'
-						}
-					}),
-					{ status: 500 }
-				);
-			}
+			if ((await findToken(sentToken, user.token)) !== null)
+				return replyError('ok', 'Already logged in.');
 
 		const isValid = await bcrypt.compare(password, user.password);
-		if (!isValid) {
-			return new Response(
-				JSON.stringify({
-					body: {
-						message: `Password is incorrect.`,
-						error: 'password'
-					}
-				}),
-				{ status: 500 }
-			);
-		}
+		if (!isValid) return replyError('password', 'Password is incorrect.');
 
-		const token = bcrypt.hashSync(uuid.v4(), 10);
-		const hashedToken = await bcrypt.hash(token, await bcrypt.genSalt(10));
+		const token = await randomHash();
+		const hashedToken = await hash(token);
 
 		await prisma.user.update({
-			where: {
-				id: user.id
-			},
-			data: {
-				token: [...user.token, hashedToken]
-			}
+			where: { id: user.id },
+			data: { token: [...user.token, hashedToken] }
 		});
 
 		const baseDate = Date.now();
 		const tokenCookie = makeCookie('token', token, baseDate);
 		const usernameCookie = makeCookie('username', username, baseDate);
 
-		return new Response(JSON.stringify({ body: { message: `OK`, usernameCookie } }), {
-			headers: { 'Set-Cookie': tokenCookie }
-		});
+		return replyWithCookies('OK', usernameCookie, tokenCookie);
 	} catch (error) {
-		return new Response(
-			JSON.stringify({
-				body: {
-					message: `${error}`,
-					error: 'unknown'
-				}
-			}),
-			{ status: 500 }
-		);
+		return replyError('unknown', `${error}`);
 	}
 };
 
@@ -120,76 +56,29 @@ export const GET: RequestHandler = async ({ url, request }) => {
 	try {
 		const sentToken = getRequestCookie(request, 'token');
 		const who = getRequestCookie(request, 'username');
-		const removeTokens = url.searchParams.has('all');
+		const removeAll = url.searchParams.has('all');
 
-		if (who === null || sentToken === null) {
-			return new Response(
-				JSON.stringify({
-					body: {
-						message: 'Not logged in.',
-						error: 'ok'
-					}
-				}),
-				{ status: 500 }
-			);
-		}
+		if (who === null || sentToken === null) return replyError('ok', 'Not logged in.');
 
 		const baseDate = Date.now();
 		const tokenCookie = makeCookie('token', null, baseDate);
 		const usernameCookie = makeCookie('username', null, baseDate);
 
-		const user = await prisma.user.findFirst({
-			where: {
-				username: who
-			}
-		});
+		const checked = await checkLogin(who, sentToken);
+		if (checked === null)
+			return replyWithCookies('Removing illegal login.', usernameCookie, tokenCookie);
 
-		if (user === null) {
-			return new Response(
-				JSON.stringify({
-					body: {
-						message: 'Removing illegal login.',
-						usernameCookie
-					}
-				}),
-				{ headers: { 'Set-Cookie': tokenCookie } }
-			);
-		}
-
-		const foundToken = await findToken(sentToken, user.token);
-		if (foundToken === null) {
-			return new Response(
-				JSON.stringify({
-					body: {
-						message: 'Removing illegal login.',
-						usernameCookie
-					}
-				}),
-				{ headers: { 'Set-Cookie': tokenCookie } }
-			);
-		}
+		let newTokens: string[];
+		if (removeAll) newTokens = [];
+		else newTokens = checked.user.token.filter((token) => token !== checked.foundToken);
 
 		await prisma.user.update({
-			where: {
-				id: user.id
-			},
-			data: {
-				token: removeTokens ? [] : user.token.filter((token) => token !== foundToken)
-			}
+			where: { id: checked.user.id },
+			data: { token: newTokens }
 		});
-		
-		return new Response(JSON.stringify({ body: { message: `OK`, usernameCookie } }), {
-			headers: { 'Set-Cookie': tokenCookie }
-		});
+
+		return replyWithCookies('OK', usernameCookie, tokenCookie);
 	} catch (error) {
-		return new Response(
-			JSON.stringify({
-				body: {
-					message: `${error}`,
-					error: 'unknown'
-				}
-			}),
-			{ status: 500 }
-		);
+		return replyError('unknown', `${error}`);
 	}
 };
