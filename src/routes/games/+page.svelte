14b6996c78930648a908c5a18e9da38a4af2b436
Trubiso <trubiso@users.noreply.github.com>
2023-08-05 14:07:38 +0200

    games page

---
+++ src/routes/games/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import './style.scss';
+</script>
+
+<main class="games">
+	<h1>WIP</h1>
+	<p>...what is this even supposed to be</p>
+	<p>have a funny duck for the inconvenience</p>
+	<div class="duck">
+		<img src="https://random-d.uk/api/randomimg" alt="the duck didn't even load :(" />
+	</div>
+</main>
