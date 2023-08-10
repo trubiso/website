b315e224e39f0d07d9cba915eb48c7fe45452f88
Trubiso <trubiso@users.noreply.github.com>
2023-08-10 20:45:21 +0200

    moved tests to /test

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	const pages = ['hsv', 'login'];
+</script>
+
+<main>
+	<h1>tests</h1>
+	<ul>
+		{#each pages as page}
+			<li>
+				<a href="/test/{page}">{page}</a>
+			</li>
+		{/each}
+	</ul>
+</main>
