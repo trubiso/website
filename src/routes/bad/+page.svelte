e0e1578fdf6c979e6506eb8573b3af4737835b52
Trubiso <trubiso@users.noreply.github.com>
2023-08-23 19:43:47 +0200

    test shorthand

--- src/routes/bad/+page.svelte
+++ src/routes/bad/+page.svelte
@@ -1,3 +1,5 @@
 <main class="bad">
-	nothing to see here... <span style="color:var(--bg)">yet</span>
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	nothing to see here...
+	<span style="color:var(--bg);cursor:default;" on:click={() => location.assign('/test')}>yet</span>
 </main>
